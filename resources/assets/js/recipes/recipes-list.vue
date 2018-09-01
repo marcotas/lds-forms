8cf9c62003f039588cd337c84829f2688a51cfb1
Marco Tulio de Avila Santos <marcotulio.avila@gmail.com>
2018-08-31 21:32:37 -0300

    [example] action to recipes list

--- resources/assets/js/recipes/recipes-list.vue
+++ resources/assets/js/recipes/recipes-list.vue
@@ -1,5 +1,5 @@
 <template lang="pug">
-    div
+    section
         h3.clearfix Receitas
 
         mt-table(
@@ -9,12 +9,14 @@
             default-sort="name",
             :options="tableOptions")
             template(slot="top-actions")
-                button.btn.btn-primary.float-right.text-gray-500
+                button.btn.btn-primary.float-right.text-gray-500.shadow-sm
                     i.fa.fa-plus.mr-2
                     | Nova receita
 
             template(slot="status", slot-scope="prop")
                 | {{ prop.row.status === 'active' ? 'Ativo' : 'Inativo' }}
+
+        confirmation(ref="confirmation")
 </template>
 
 <script>
@@ -39,9 +41,34 @@ export default {
                 filters: [
                     { field: 'status', label: 'Status Ativo', value: 'active' },
                     { field: 'status', label: 'Status Inativo', value: 'inactive' },
-                ]
-            }
+                ],
+                actions: [
+                    {
+                        name: 'Activate Recipe',
+                        callback: this.action.bind(this),
+                    },
+                    {
+                        name: 'Reiniciar',
+                        callback: this.action.bind(this),
+                        confirmation: {
+                            message: 'Tem certeza que deseja realizar esta ação?',
+                            title: 'Confirmação',
+                        },
+                    },
+                ],
+            },
         };
     },
-}
+
+    methods: {
+        async action(selected) {
+            return new Promise(resolve =>
+                setTimeout(() => {
+                    console.log('ação realizada para os ids', selected);
+                    resolve();
+                }, 1000),
+            );
+        },
+    },
+};
 </script>
